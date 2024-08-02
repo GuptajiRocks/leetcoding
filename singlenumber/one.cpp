f3815082d5901dbd40547cd0b38ee97e695bed04
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-02 11:19:54 +0530

    hello world basic

---
+++ singlenumber/one.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    int n;
+
+    cin >> n;
+
+    cout << "The number entered is: " << n;
+
+    return 0;
+}
