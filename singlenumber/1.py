f2947e497c548e6329a32b78922b471571115022
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-03 19:33:55 +0530

    still trying single number lol

--- singlenumber/1.py
+++ singlenumber/1.py
@@ -3,16 +3,13 @@ from typing import List
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         td = dict()
-        res = 0
         for i in range(len(nums)):
             count = 0
-            for j in nums:
-                if nums[i] == j:
+            for j in range(len(nums)):
+                if nums[i] == nums[j]:
                     count += 1
                 
             td[nums[i]] = count
-        
-        
 
         for k in td:
             if td[k] == 1:
@@ -21,5 +18,5 @@ class Solution:
         return 0          
 
 x = Solution()
-res = x.singleNumber([2,2,1])
+res = x.singleNumber([4,1,2,1,2])
 print(res)
--- singlenumber/tryone.java
+++ singlenumber/tryone.java
@@ -2,11 +2,23 @@ package singlenumber;
 import java.util.*;
 public class tryone {
     public static int single(int[] arr) {
-        int[] temp = new int[arr.length];
-        // ArrayList s1 = Arrays.asList(arr);
+        LinkedHashMap<Integer,Integer> s1 = new LinkedHashMap<>();
+        for (int i = 0; i<arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j<arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count = count + 1;
+                }
+            }
 
-
+            s1.put(i,count);
+        }
         
+        for (int i = 0; i<s1.size(); i++) {
+            System.out.println();
+        }
+        
+                
 
         return 0;
     }
