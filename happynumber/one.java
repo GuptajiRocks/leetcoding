e5fbd5f268733eec4bc3fa057cdcc3d3f4777bae
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-13 15:13:02 +0530

    still not working

---
+++ happynumber/one.java
@@ -0,0 +1,10 @@
+package happynumber;
+import java.util.Scanner;
+public class one {
+    public static void checkHappy(int n) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+                
+    }
+}
--- happynumber/one.py
+++ happynumber/one.py
@@ -3,11 +3,11 @@ def checkhappy(x):
     while (temp != 1):
         for i in range(len(x)):
             temp = int(x[0])**2
-            if temp == 1:
-                return "Happy"
-            else:
-                # temp = 0
-                return calcpower(int(x[i]))
+        if temp == 1:
+            return "Happy"
+        else:
+            temp = 0
+            return calcpower(int(x[i]))
         
 
 def calcpower(y):
