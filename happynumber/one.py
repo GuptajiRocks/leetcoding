c64c8671f228bb5931c23745ae9192581b0288ce
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-13 13:05:51 +0530

    wrong logic commit

--- happynumber/one.py
+++ happynumber/one.py
@@ -1,6 +1,18 @@
 def checkhappy(x):
-    while (x == 1):
+    temp = 0
+    while (temp != 1):
+        for i in range(len(x)):
+            temp = int(x[0])**2
+            if temp == 1:
+                return "Happy"
+            else:
+                # temp = 0
+                return calcpower(int(x[i]))
         
 
+def calcpower(y):
+    return y**2
 
 x = str(input("Enter the number: "))
+res = checkhappy(x)
+print(res)
