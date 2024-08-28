f64715c1232e79500fb1e38bdaf07b74d02f5fd5
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-28 08:40:59 +0530

    little thing added

--- strivera2z/sorting/mergesort/one.cpp
+++ strivera2z/sorting/mergesort/one.cpp
@@ -16,10 +16,12 @@ void merge(vector<int>& arr, int left,
     vector<int> L(n1), R(n2);
 
     // Copy data to temp vectors L[] and R[]
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < n1; i++) {
         L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
+    }
+    for (int j = 0; j < n2; j++) {
         R[j] = arr[mid + 1 + j];
+    }
 
     int i = 0, j = 0;
     int k = left;
