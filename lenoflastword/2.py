de5caa8c550905ec53e7fac48adb64328e198af0
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-29 16:18:33 +0530

    pushed random

--- lenoflastword/2.py
+++ lenoflastword/2.py
@@ -1,14 +1,19 @@
+def les():
+    l = x.split(" ")
+    count = 0
+
+    l.reverse()
+    global temp
+
+    for i in l:
+        if i != "":
+            temp = i
+            break
+
+    return len(temp)
+
+
 x = "   fly me to   the moon   "
-l = x.split(" ")
-count = 0
 
-l.reverse()
-global temp
-
-for i in l:
-    if i != "":
-        temp = i
-        break
-
-print(len(temp))
+leeng = les(x)
 
