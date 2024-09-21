80028556e3eacea5e35ef4297be3952a0ac3dcca
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-22 00:13:54 +0530

    decrement while try

---
+++ codeforces/2008A/2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Trying decrement operator while loop
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << "This is count: " << t << endl;
+    }
+}
