8fff1582bbe453f1f126a83bc20bf1b449835d2b
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-01-29 10:51:18 +0530

    leetcode 707

---
+++ designLL/one.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string arr[] = {"MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"};
+    int arr[][] = {{}, {1}, {3}, {1,2}, {1}, {1}, {1}};
+    for (string i : arr) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
