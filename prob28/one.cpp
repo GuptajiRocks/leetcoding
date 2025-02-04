8bd77281e3d32b8a992e9f4ad32a59dbf1efd16d
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-04 14:58:04 +0530

    working fine local

--- prob28/one.cpp
+++ prob28/one.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    string haystack = "butsad";
-    string needle = "sad";
-    int res = 0;
-    int count = 0;
+    string haystack = "baabbbbababbbabab";
+    string needle = "abbab";
+    int res = -1;
 
     for (int i = 0; i < haystack.length(); i++) {
         if (haystack[i] == needle[0]) {
+            int count = 0;
+            int k = i;
             for (int j = 0; j < needle.length(); j++) {
-                int k = i;
                 if (haystack[k] == needle[j]) {
                     count++;
                 }
