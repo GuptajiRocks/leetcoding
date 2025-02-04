d848419851fd44ea313d07abdadc77486a0ace43
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-04 12:24:38 +0530

    cpp now, py done

--- prob28/one.cpp
+++ prob28/one.cpp
@@ -2,10 +2,29 @@
 using namespace std;
 
 int main() {
-    string haystack = "sadbutsad";
+    string haystack = "butsad";
     string needle = "sad";
+    int res = 0;
+    int count = 0;
 
     for (int i = 0; i < haystack.length(); i++) {
-        cout << haystack[i];
+        if (haystack[i] == needle[0]) {
+            for (int j = 0; j < needle.length(); j++) {
+                int k = i;
+                if (haystack[k] == needle[j]) {
+                    count++;
+                }
+                k++;
+            }
+
+            if (count == needle.length()) {
+                res = i;
+                break;
+            }
+        }
     }
+
+    cout << res << endl;
+
+    return 0;
 }
