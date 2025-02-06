04df4a8778748204edf5d2cbecdd3f8b9cbd28bf
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-06 08:44:55 +0530

    2056A CF try

---
+++ codeforces/2056A/one.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int ops, sl;
+    cin >> ops >> sl;
+    
+    
+    if (ops == 1) {
+        cout << 4*sl << endl;
+    }
+
+    return 0;
+}
