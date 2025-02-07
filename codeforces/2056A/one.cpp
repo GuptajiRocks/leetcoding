9c6f3ef47807cc94ec62ce82a368825cb288aa44
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-07 16:46:03 +0530

    2056a CONTINUE

--- codeforces/2056A/one.cpp
+++ codeforces/2056A/one.cpp
@@ -6,11 +6,19 @@ int main() {
     cin >> n;
     int ops, sl;
     cin >> ops >> sl;
-    
-    
-    if (ops == 1) {
-        cout << 4*sl << endl;
+
+    int arr[n][2];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i][0] >> arr[i][1];
     }
+
+    vector<pair<int, int>> jesus;
+
+    int xsum = arr[0][0];
+    int ysum = arr[0][1];
+
+    jesus.push_back({12,12});
 
     return 0;
 }
