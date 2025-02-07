a77a75c2511c850d203dffced345c01e739c511f
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-07 17:01:02 +0530

    previous array completion

--- codeforces/2056A/one.cpp
+++ codeforces/2056A/one.cpp
@@ -1,11 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printPair(vector<pair<int,int>> p, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << p[i].first << " " << p[i].second << endl;
+    }
+    cout << endl;
+}
+
 int main() {
-    int n;
-    cin >> n;
+    // int n;
+    // cin >> n;
     int ops, sl;
     cin >> ops >> sl;
+    int n = ops;
 
     int arr[n][2];
 
@@ -15,10 +23,25 @@ int main() {
 
     vector<pair<int, int>> jesus;
 
-    int xsum = arr[0][0];
-    int ysum = arr[0][1];
+    int xsum = 0;
+    int ysum = 0;
 
-    jesus.push_back({12,12});
+    int co = 0;
+
+    do {
+        xsum = xsum + arr[co][0];
+        ysum = ysum + arr[co][1];
+        jesus.push_back({xsum, ysum});
+        co = co+1;
+    } while (co < n);
+
+    cout << "Pairs have been entered" << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "Now here is the updated counter of x, y" << endl;
+    cout << endl;
+
+    printPair(jesus, n);
 
     return 0;
 }
