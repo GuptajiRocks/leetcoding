645cf9c045b7a0fef31f0765fee1182badf7d632
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-08 22:46:30 +0530

    q2 LC, local working good

--- add_two_num-LL/three.cpp
+++ add_two_num-LL/three.cpp
@@ -11,15 +11,46 @@ struct Node {
 };
 
 int main() {
-    Node* head = new Node(4);
-    head->next = new Node(3);
-    head->next->next = new Node(7);
+    vector<int> arr1;
+    vector<int> arr2;
+    Node* head1 = new Node(4);
+    head1->next = new Node(3);
+    head1->next->next = new Node(7);
 
-    Node* trav = head;
-    while (trav != nullptr) {
-        cout << trav->data << " ";
-        trav = trav->next;
+    Node* head2 = new Node(8);
+    head2->next = new Node(3);
+    head2->next->next = new Node(7);
+    head2->next->next->next = new Node(3);
+    
+    Node* trav1 = head1;
+    Node* trav2 = head2;
+
+    while (trav1 != nullptr) {
+        arr1.push_back(trav1->data);
+        trav1 = trav1->next;
     }
 
+    while (trav2 != nullptr) {
+        arr2.push_back(trav2->data);
+        trav2 = trav2->next;
+    }
+
+    int co = 1;
+    int num1 = 0;
+    int num2 = 0;
+
+    for (int num : arr1) {
+        num1 = num1 + (co*num);
+        co = co*10;
+    }
+
+    co = 1;
+
+    for (int num : arr2) {
+        num2 = num2 + (co*num);
+        co = co*10;
+    }
+
+    cout << num1+num2 << endl;
     return 0;
 }
