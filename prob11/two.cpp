c3f0d745fc0d81605baceb1a0716895d950836df
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-06-21 20:22:32 +0530

    NULL and nullptr cleared

---
+++ prob11/two.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int *a = NULL;
+
+    if (a == nullptr) { 
+        cout << "Nullpointer";
+    } else {
+        cout << "Get out gooner";
+    }
+
+    return 0;
+}
