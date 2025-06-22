9a15b9a02fa0612d485ae832d7403704e676158c
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-06-23 00:01:57 +0530

    rotate LL n rotate start

---
+++ rotateLL_61/two.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int c) {
+        data = c;
+        next = nullptr;
+    }
+};
+
+
+int main() { 
+
+}
