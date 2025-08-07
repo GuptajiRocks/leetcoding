c936d95f33ea99c3eb49f54910d16216bbc0aba4
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-08-07 12:18:41 +0530

    cses start

---
+++ aa_cses/weird_algo.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void jesus(int n) { 
+    cout << n << " ";
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n/2;
+            cout << n << " ";
+        } else {
+            n = (n*3) + 1;
+            cout << n << " ";
+        }
+    }
+}
+
+int main() {
+    int num;
+    cin >> num;
+
+    jesus(num);
+
+    return 0;
+}
