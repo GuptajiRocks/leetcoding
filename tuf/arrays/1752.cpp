767feaec442b41a8c48571e1926bdc5be2a93fee
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2026-01-23 00:45:40 +0530

    arrays ?

---
+++ tuf/arrays/1752.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool pups(vector<int> arr, int n) {
+    bool curr;
+
+    for (int i = 0; i < n; i++) {
+        
+    }
+}
+
+int main() {
+    vector<int> arr = {3, 4, 5, 1, 2};
+    int n = arr.size();
+
+    bool res = pups(arr, n);
+
+    cout << res << endl;
+
+    return 0;
+}
